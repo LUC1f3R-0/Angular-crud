9c368c89fd230504355037ed612eba05d97e22a0
LUC1f3R-0 <thusharathilan123@gmail.com>
2026-03-18 01:16:15 +0530

    added tailswind

---
+++ 02/src/app/app.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+  { path: '', loadComponent: ()=> import('./pages/home/home.component').then(c => c.HomeComponent) },
+  { path: 'about', loadComponent: ()=> import('./pages/about/about.component').then(c => c.AboutComponent)}
+];

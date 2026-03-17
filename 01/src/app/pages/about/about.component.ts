70127db9f171b4927f6e77bb59460e962808dc77
LUC1f3R-0 <thusharathilan123@gmail.com>
2026-03-17 19:56:03 +0530

    added the conolse.log

--- 01/src/app/pages/about/about.component.ts
+++ 01/src/app/pages/about/about.component.ts
@@ -1,12 +1,19 @@
 import { Component } from "@angular/core";
+import { FormControl, ReactiveFormsModule } from "@angular/forms";
 import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'about-app',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css'],
 })
 
-export class AboutComponent { }
+export class AboutComponent {
+  name = new FormControl('', {nonNullable: true});
+  
+  clicked(){
+    console.log("clicked")
+  }
+}

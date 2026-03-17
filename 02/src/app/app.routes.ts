30447b8bc0e022faa3b978f9bd25da2540b8c353
LUC1f3R-0 <thusharathilan123@gmail.com>
2026-03-18 01:18:10 +0530

    changed the toute method

--- 02/src/app/app.routes.ts
+++ 02/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
 
 export const routes: Routes = [
-  { path: '', loadComponent: ()=> import('./pages/home/home.component').then(c => c.HomeComponent) },
-  { path: 'about', loadComponent: ()=> import('./pages/about/about.component').then(c => c.AboutComponent)}
+  { path: '', component: HomeComponent},
+  { path: 'about', component: AboutComponent}
 ];

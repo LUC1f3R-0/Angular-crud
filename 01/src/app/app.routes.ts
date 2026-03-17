411753d5283c211efbd80fdfc1db89d1bdb43603
LUC1f3R-0 <thusharathilan123@gmail.com>
2026-03-17 14:03:46 +0530

    created the firsat route

--- 01/src/app/app.routes.ts
+++ 01/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: "", loadComponent: ()=> import('./pages/home/home.component').then(c=>c.HomeComponent)
+  }
+];
